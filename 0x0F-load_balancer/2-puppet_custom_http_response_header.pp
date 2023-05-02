2dce2bf290e276f2fa5cd41504c4448cb88d1583
Budabos <opiyocaroline20@gmail.com>
2023-05-02 10:24:51 +0000

    load_balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,28 @@
-# Installs a Nginx server with custom HTTP header
+# Installs Nginx server and add custom header
 
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+	provider => shell,
+	command  => 'sudo apt-get -y update',
+	before   => Exec['install nginx'],
+
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+exec {'install nginx':
+	provider => shell,
+	command  => 'sudo apt-get -y install nginx',
+	before   => Exec['add_header'],
+
 }
 
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+exec {'add_header':
+	provider     => shell,
+	environment  => ["HOST=${HOSTNAME}"],
+	command      => 'echo "add_header X-Served-By $hostname;" | sudo tee -a /etc/nginx/nginx.conf > /dev/null',
+
 }
 
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+exec {'restart nginx':
+	provider  => shell,
+	command   => 'sudo service nginx restart',
+
 }
